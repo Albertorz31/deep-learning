12f60015099acc642e7632ed65501a47aef57df8
Albertorz31 <alberto.rodriguez.z@usach.cl>
2021-06-03 18:28:55 +0200

    comments

--- Ex.2/src_to_implement/Optimization/Optimizers.py
+++ Ex.2/src_to_implement/Optimization/Optimizers.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Sgd:
     def __init__(self, learning_rate):
         self.learning_rate = float(learning_rate)
@@ -9,11 +12,13 @@ class Sgd:
         return update_w
 
     class SgdWithMomentum:
+        # Parameter update based on current and past gradients: momentum
         def __init__(self, learning_rate, momentum_rate):
             self.learning_rate = float(learning_rate)
             self.momentum_rate = float(momentum_rate)
 
         def calculate_update(self, weight_tensor, gradient_tensor):
+            # v_k = momentum* v_k-1 - n*gradient
             try:
                 self.v = self.momentum_rate * self.v - self.learning_rate * gradient_tensor
             except:
@@ -22,6 +27,7 @@ class Sgd:
             return weight_tensor + self.v
 
     class Adam:
+        # parameter update based on current and past gradients:
         def __init__(self, learning_rate, mu, rho):
             self.learning_rate = float(learning_rate)
             self.mu = float(mu)
@@ -33,7 +39,9 @@ class Sgd:
 
         def calculate_update(self, weight_tensor, gradient_tensor):
             try:
+                # v_k = mu*v_k-1 + (1-mu)*gradient
                 self.v = self.mu * self.v + (1 - self.mu) * gradient_tensor
+                # r_k = rho*r_k-1 + (1-rho)* gradient x gradient (Multiply arguments element-wise)
                 self.r = self.rho * self.r + (1 - self.rho) * np.multiply(gradient_tensor, gradient_tensor)
             except:
                 self.v = 0
@@ -41,9 +49,10 @@ class Sgd:
                 self.v = self.mu * self.v + (1 - self.mu) * gradient_tensor
                 self.r = self.rho * self.r + (1 - self.rho) * np.multiply(gradient_tensor, gradient_tensor)
 
+            # Bias correction
             v_hat_k = self.v / (1 - self.mu ** self.count)
             r_hat_k = self.r / (1 - self.rho ** self.count)
             self.count += 1
-            return weight_tensor - self.learning_rate * (v_hat_k + np.finfo(np.float).eps) / (
-                    np.sqrt(r_hat_k) + np.finfo(np.float).eps)
-
+            w_k1 = weight_tensor - self.learning_rate * (v_hat_k + np.finfo(np.float).eps) / (
+                        np.sqrt(r_hat_k) + np.finfo(np.float).eps)
+            return w_k1
